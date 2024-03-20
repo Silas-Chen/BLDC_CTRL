f9ff09ef77cda9a5358deaec1573d625ecc5812f
Silas-Chen <ubik.3141@gmail.com>
2024-03-21 01:11:59 +0800

    4 channels, with RAM overflow.

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2024 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -26,7 +26,8 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include "string.h"
+#include "stdint.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -41,13 +42,26 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-
+#define NPT 256 // Number of FFT points
+#define CHANNELNUM 4
+#define CHANNELNUM__2 (CHANNELNUM / 2)
+#define PSC 18   // According to STM32CubeMX configuration
+#define ARR 1000 // According to STM32CubeMX configuration
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
-
+uint32_t ADC_BUFFER[NPT][CHANNELNUM__2];
+uint32_t FFT_IN[CHANNELNUM][NPT];
+uint32_t FFT_OUT[CHANNELNUM][NPT];
+uint32_t FFT_MAG[CHANNELNUM][NPT / 2];
+uint8_t i = 0;
+uint8_t j = 0;
+uint8_t m = 0;
+uint8_t n = 0;
+float FS = 180000000.0 / ((float)ARR * (float)PSC);
+float FREQ[CHANNELNUM] = {0};
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -94,17 +108,19 @@ int main(void)
   MX_TIM3_Init();
   MX_CAN1_Init();
   /* USER CODE BEGIN 2 */
-
+    HAL_TIM_Base_Start(&htim3);
+    // HAL_TIM_Base_Start_IT(&htim3);
+    HAL_ADC_Start_DMA(&hadc1, (uint32_t *)ADC_BUFFER, NPT * CHANNELNUM); // One uint32 for two channels, there are 4 channels, so we need NPT*CHANNELNUM
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+    while (1)
+    {
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-  }
+    }
   /* USER CODE END 3 */
 }
 
@@ -162,7 +178,62 @@ void SystemClock_Config(void)
 }
 
 /* USER CODE BEGIN 4 */
+// ADC-DMA interrupt callback function
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)
+{
+    // Stop ADC-DMA
+    HAL_ADC_Stop_DMA(&hadc1);
 
+    for (i = 0; i < NPT; i++)
+    {
+        // for (m = 0; m < CHANNELNUM; m++)
+        // {
+    //         // if ((m % 2) == 0)
+    //         // {
+    //         //     FFT_IN[m][i] = (ADC_BUFFER[i][0] << 16);
+    //         //     // FFT_IN[m][i] = (FFT_IN[m][i] << 16); // FFT_IN is 32-bit, first 16-bit should be real number, and the second 16-bit should be imaginary number
+    //         // }
+    //         // else
+    //         // {
+    //         //     FFT_IN[m][i] = (ADC_BUFFER[i][1] >> 16);
+    //         //     FFT_IN[m][i] = (FFT_IN[m][i] << 16); // FFT_IN is 32-bit, first 16-bit should be real number, and the second 16-bit should be imaginary number
+    //         // }
+    //         // switch (m)
+    //         // {
+    //         // case 0:
+                FFT_IN[0][i] = (ADC_BUFFER[i][0] << 16);
+    //             // FFT_IN[0][i] = (FFT_IN[0][i]<<16); // FFT_IN is 32-bit, first 16-bit should be real number, and the second 16-bit should be imaginary number
+    //         //     break;
+    //         // case 1:
+    //             FFT_IN[1][i] = (ADC_BUFFER[i][0] >> 16);
+    //             FFT_IN[1][i] = (FFT_IN[1][i] << 16); // FFT_IN is 32-bit, first 16-bit should be real number, and the second 16-bit should be imaginary number
+    //         //     break;
+    //         // case 2:
+    //             FFT_IN[2][i] = (ADC_BUFFER[i][1] << 16);
+    //             // FFT_IN[2][i] = (FFT_IN[2][i]<<16); // FFT_IN is 32-bit, first 16-bit should be real number, and the second 16-bit should be imaginary number
+    //         //     break;
+    //         // case 3:
+    //             FFT_IN[3][i] = (ADC_BUFFER[i][1] >> 16);
+    //             FFT_IN[3][i] = (FFT_IN[3][i] << 16); // FFT_IN is 32-bit, first 16-bit should be real number, and the second 16-bit should be imaginary number
+    //             // break;
+    //         // default:
+    //         //     // Error
+    //         //     break;
+            // }
+        // }
+    }
+
+    // Reset i, j, m, n
+    m = 0;
+    n = 0;
+    i = 0;
+    j = 0;
+
+    // getFFT_MAG();
+    // getMAX_FFT_MAG_FREQ();
+    // Restart ADC-DMA
+    HAL_ADC_Start_DMA(&hadc1, (uint32_t *)ADC_BUFFER, NPT * CHANNELNUM);
+}
 /* USER CODE END 4 */
 
 /**
@@ -172,11 +243,11 @@ void SystemClock_Config(void)
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while (1)
+    {
+    }
   /* USER CODE END Error_Handler_Debug */
 }
 
@@ -191,8 +262,8 @@ void Error_Handler(void)
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+    /* User can add his own implementation to report the file name and line number,
+       ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # BLDC_Motor_Control
+
 with STM32F427IIH6
+
 including ADC, DMA, FFT, CAN(communicate with upper monitor).
